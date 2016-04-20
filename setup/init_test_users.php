c72cb0f7838ab276a40f8bdd6d1550f501f3f5eb
samhargreaves <sam@trg-uk.net>
2016-04-20 16:13:41 +0300

    Initial User Setup File Fix

--- setup/init_test_users.php
+++ setup/init_test_users.php
@@ -1,7 +1,7 @@
 <?php
 	// Include Necessary Files
-	include dirname(__FILE__)."/config.php";
-	include dirname(__FILE__)."/standart_connection.php";
+	include dirname(__FILE__)."/../config.php";
+	include dirname(__FILE__)."/../standart_connection.php";
 	$i= 1;
 	while($i <= 5){
 		$queryToInsert= "INSERT INTO users(username, password, full_name) VALUES('test".$i."', '".md5('test'.$i)."', 'test".$i."')";
